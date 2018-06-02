8ad66bba767c32a818a3a6f4c8f8e54893969f46
Oziel-Silva <oziel.service@live.com>
2018-06-02 12:50:54 -0300

    code update

--- filter_qrs.m
+++ filter_qrs.m
@@ -3,12 +3,13 @@ clear all; close all; clc;
 load('../sinais/ecg_grupo1/ECG_1.mat','fs','x');
 
 x = x(: ,1);
+x = x - mean(x);
 n = 0 : length(x) -1;
 t = n;
 
 
 %Cálculo da Tranformada de Fourier
-X1 = fft(x) - mean(x);
+X1 = fft(x) ;
 
 %criando eixo de frenquências
 f = linspace(-0.5, 0.5, length(X1))*fs;
@@ -62,33 +63,34 @@ xlabel('frequência (Hz)')
 ylabel('|X_jw(f)|')
 
 %separando os picos QRS (10 amostras de QRS)
-qrs1 = y4(2352 : 2385)';
-qrs2 = y4(2647 : 2673)';
-qrs3 = y4(2928 : 2959)';
-qrs4 = y4(3216 : 3242)';
-qrs5 = y4(3498 : 3526)';
-qrs6 = y4(3793 : 3821)';
-qrs7 = y4(4028 : 4054)';
-qrs8 = y4(4385 : 4418)';
-qrs9 = y4(4690 : 4718)';
-qrs10 = y4(4984 : 5008)';
+qrs{1} = y4(2352 : 2385)';
+qrs{2} = y4(2647 : 2673)';
+qrs{3} = y4(2928 : 2959)';
+qrs{4} = y4(3216 : 3242)';
+qrs{5} = y4(3498 : 3526)';
+qrs{6} = y4(3793 : 3821)';
+qrs{7} = y4(4028 : 4054)';
+qrs{8} = y4(4385 : 4418)';
+qrs{9} = y4(4690 : 4718)';
+qrs{10} = y4(4984 : 5008)';
 
-QRS1 = (abs(fft([qrs1 zeros(1 ,abs(length(qrs1) - 1000))]))).^2;
-QRS2 = (abs(fft([qrs2 zeros(1 ,abs(length(qrs2) - 1000))]))).^2;
-QRS3 = (abs(fft([qrs3 zeros(1 ,abs(length(qrs3) - 1000))]))).^2;
-QRS4 = (abs(fft([qrs4 zeros(1 ,abs(length(qrs4) - 1000))]))).^2;
-QRS5 = (abs(fft([qrs5 zeros(1 ,abs(length(qrs5) - 1000))]))).^2;
-QRS6 = (abs(fft([qrs6 zeros(1 ,abs(length(qrs6) - 1000))]))).^2;
-QRS7 = (abs(fft([qrs7 zeros(1 ,abs(length(qrs7) - 1000))]))).^2;
-QRS8 = (abs(fft([qrs8 zeros(1 ,abs(length(qrs8) - 1000))]))).^2;
-QRS9 = (abs(fft([qrs9 zeros(1 ,abs(length(qrs9) - 1000))]))).^2;
-QRS10 = (abs(fft([qrs10 zeros(1 ,abs(length(qrs10) - 1000))]))).^2;
+for k= 1: 10
+QRS{k} = abs(fft(qrs{k}, 1000)).^2;
+end
 
 mean_qrs = zeros(1,1000);
 
 % tirando a média
 for i = 1 : 1000
-    mean_qrs(i) = (QRS1(i) + QRS2(i) + QRS3(i) +QRS4(i) + QRS5(i) + QRS6(i) + QRS7(i) + QRS8(i) + QRS9(i) + QRS10(i) )/10; 
+    for k = 1 : 10
+        v = QRS{k};
+        v = v(i);
+        mean_qrs(i) = mean_qrs(i) + v/10; 
+    end
 end
+f= linspace (-0.5, 0.5, length(mean_qrs))*fs; 
 figure
-plot(mean_qrs)
+
+plot(f,fftshift(mean_qrs));
+a = axis();
+axis ([0 50 a(3) a(4)]);
